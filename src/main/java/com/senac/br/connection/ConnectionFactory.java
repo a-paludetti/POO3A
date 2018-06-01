d1bb861648d513a67f64b4bbd54e5c57b1426ec2
a-paludetti <a.paludetti@gmail.com>
2018-06-01 17:01:26 -0300

    Tela de pesquisa, relatório de contas, inicio da tela de resumo.

---
+++ src/main/java/com/senac/br/connection/ConnectionFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.br.connection;
+
+/**
+ *
+ * @author Red
+ */
+public class ConnectionFactory {
+//    private static final String DRIVER = "com.mysql.jdbc.Driver";
+//    private static final String URL = "jdbc:mysql://localhost:3306/ljgames?useSSL=false";
+//    private static final String USER = "me";
+//    private static final String PASS = "hjp310780";
+}
